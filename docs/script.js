8252470dad397e1a635b5a701bf084130d8cccb6
sabeerbikba <sabeerbikba02@gmail.com>
2024-10-10 13:32:29 +0530

    test docs folder

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeGreeting').addEventListener('click', function () {
+   const greetingElement = document.getElementById('greeting');
+   greetingElement.textContent = 'Hello, Universe!';
+});
