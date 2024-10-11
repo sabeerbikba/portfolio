b542f32bc8c3f8e030f0fb58c6257ab0d5f97dd0
sabeerbikba <sabeerbikba02@gmail.com>
2024-10-11 16:44:40 +0530

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
-// import { isVercel } from "@/constants/envexport"; // TODO: is working fine 
 const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-   // output: 'export',
-   // images: {
-   //    unoptimized: true,
-   // },
-   output: isVercel ? undefined : 'export',
-   images: {
-      unoptimized: isVercel ? true : false,
-   },
-};
+const nextConfig = {};
 
 export default nextConfig;
