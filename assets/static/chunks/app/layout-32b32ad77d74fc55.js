a64a47ee2af675944b80c92d4c83c77493aa4727
sabeerbikba <sabeerbikba02@gmail.com>
2024-10-11 13:01:00 +0530

    Updates

---
+++ assets/static/chunks/app/layout-32b32ad77d74fc55.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{9442:function(n,e,u){Promise.resolve().then(u.t.bind(u,8226,23))},8226:function(){}},function(n){n.O(0,[738,985,330,744],function(){return n(n.s=9442)}),_N_E=n.O()}]);
--- src/constants/envexport.ts
+++
@@ -1,8 +0,0 @@
-const BASE_URL = process.env.BASE_URL as string;
-const isVercel: boolean = process.env.VERCEL === '1'; // not used anywhere 
-
-
-export {
-   isVercel,
-   BASE_URL,
-};
